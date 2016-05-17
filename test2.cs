e72c12595f6cd4a7265ee3b060fa1326a65b4e5e
RenosBardhis <bardis15@aueb.gr>
2016-05-17 18:05:53 +0300

    Add files via upload

---
+++ test2.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Renos
+ * Date: 10/1/2016
+ * Time: 10:34 μμ
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace DefaultNamespace
+{
+	
+	public class Class1
+	{
+		public Class1()
+		{
+			Console.Write("Δώσε κάποιον αριτθμό: ");
+			String s= Console.In.ReadLine();
+			int noumero= Int32.Parse(s);
+			Console.WriteLine(noumero);
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
